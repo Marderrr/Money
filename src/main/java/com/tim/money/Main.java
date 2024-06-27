cf4528fd8c75b4461e8e4cfa4ca0e20c8ab49b61
Gamesuchti <timbo0411@hotmail.com>
2024-06-27 16:24:40 +0200

    initial commit

---
+++ src/main/java/com/tim/money/Main.java
@@ -0,0 +1,18 @@
+package com.tim.money;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+    
+}
